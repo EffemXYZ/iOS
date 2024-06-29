b0bcc6bb0e132e81e8d6f3acfd48d4ec5f2316a2
Thomas Rademaker <thomas@effem.xyz>
2024-06-29 08:14:17 -0400

    fix swiftData relationship logic

--- Effem/Models/Album.swift
+++ Effem/Models/Album.swift
@@ -14,13 +14,11 @@ class Album {
     let releaseDate: Date
     let image: String
     let tracks: [Track]?
-    @Relationship(.unique, deleteRule: .cascade, inverse: \Artist.albums) let artist: Artist
     
-    init(name: String, releaseDate: Date, image: String, artist: Artist) {
+    init(name: String, releaseDate: Date, image: String) {
         self.name = name
         self.releaseDate = releaseDate
         self.image = image
-        self.artist = artist
     }
 }
 
--- Effem/Preview Content/previewContainer.swift
+++ Effem/Preview Content/previewContainer.swift
@@ -17,7 +17,7 @@ public let previewContainer: ModelContainer = {
 //        let trackOne = Track(name: "one", duration: 123, streamURL: "", artist: Artist(name: "Tom", image: "", albums: [], tracks: []), album: Album(name: "", releaseDate: .now, image: "", artist: Artist(name: "", image: "", albums: [], tracks: []), tracks: []))
         
         let artist = Artist(name: "Matchbox Twenty", image: "https://variety.com/wp-content/uploads/2023/04/Matchbox-Twenty-2023-credit-Jimmy-Fontaine.jpg?w=1000&h=562&crop=1")
-        let album = Album(name: "Yourself or Someone Like You", releaseDate: .now, image: "https://upload.wikimedia.org/wikipedia/en/4/4e/Matchbox_Twenty_-_Yourself_or_Someone_Like_You.jpg", artist: artist)
+        let album = Album(name: "Yourself or Someone Like You", releaseDate: .now, image: "https://upload.wikimedia.org/wikipedia/en/4/4e/Matchbox_Twenty_-_Yourself_or_Someone_Like_You.jpg")
         let trackOne = Track(name: "3 am", duration: 226, streamURL: "", artist: artist, album: album)
         container.mainContext.insert(trackOne)
         try container.mainContext.save()
